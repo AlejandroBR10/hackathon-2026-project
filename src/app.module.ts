6312b98741beabf13acfc521f7739e6abcbe82fa
AlejandroBR10 <alejabaldera@gmail.com>
2026-04-17 15:49:53 -0600

    Project structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PatientsModule } from './patients/patients.module';
+import { ReportsModule } from './reports/reports.module';
+import { VisionModule } from './vision/vision.module';
+import { AiModule } from './ai/ai.module';
 
 @Module({
-  imports: [],
+  imports: [PatientsModule, ReportsModule, VisionModule, AiModule],
   controllers: [AppController],
   providers: [AppService],
 })
