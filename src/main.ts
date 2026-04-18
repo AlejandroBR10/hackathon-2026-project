7f0c3c0593377756db237e05fe5613da6651f407
AlejandroBR10 <alejabaldera@gmail.com>
2026-04-17 23:09:21 -0600

    Changing app.module CORS ACTIVE

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: "*", // 🔥 para hackathon (rápido)
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
