3b2ef2b8e515d72ab9f0a173ce2fab42590c05ff
Alan Barrera <nosaboxdxdjajasalu2@gmail.com>
2026-04-17 21:17:40 -0600

    feat: YOLO model training for piece counting, inventories entity added

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { ReportsModule } from "./reports/reports.module";
 import { VisionModule } from "./vision/vision.module";
 import { AiModule } from "./ai/ai.module";
 import { DatabaseModule } from "./database/database.module";
+import { InventoriesModule } from './inventories/inventories.module';
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { DatabaseModule } from "./database/database.module";
     ReportsModule,
     VisionModule,
     AiModule,
+    InventoriesModule,
   ],
 })
 export class AppModule {}
